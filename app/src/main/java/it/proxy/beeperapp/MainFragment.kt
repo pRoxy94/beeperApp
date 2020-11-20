66d79893a9956dd73cde0aa85aea0dc941eb4d27
Rossana Laricchia <rossana.laricchia@gmail.com>
2020-11-20 11:29:35 +0100

    layout: Added fragment_search.xml and SearchFragment.kt. Updated themes.xml and strings.xml

--- app/src/main/java/it/proxy/beeperapp/MainFragment.kt
+++ app/src/main/java/it/proxy/beeperapp/MainFragment.kt
@@ -5,24 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import it.proxy.beeperapp.databinding.FragmentSearchBinding
 
-class SearchFragment : Fragment() {
-
-    private lateinit var binding: FragmentSearchBinding
+class MainFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
-        return binding.root
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {}
+            MainFragment().apply {}
     }
 }
